ece5756a5b5e7eca3f04e7ce7783ae11e7823e8a
rhuanvk <rhuanvk@gmail.com>
2022-09-24 23:05:07 -0300

    add html structure and css style

---
+++ script.js
@@ -0,0 +1,11 @@
+/*
+adicionar um numero entre 1 e 100
+exibe em tela os numeros adicionados
+botao finalizar para chamar a funçao
+funcao ira retornar:
+1 - a quantidade de numeros cadastrados
+2 - o maior numero cadastrado
+3 - o menor numero cadastrado
+4 - a soma de todos os valores
+5 - a media dos valores
+*/
