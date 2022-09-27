9226c4b88e03b9e969c47b54f7024ef99b80a76f
rhuanvk <rhuanvk@gmail.com>
2022-09-27 19:01:06 -0300

    add js scripts, analyser fully works now

--- script.js
+++ script.js
@@ -1,22 +1,65 @@
-/*
-adicionar um numero entre 1 e 100
-exibe em tela os numeros adicionados
-botao finalizar para chamar a funçao
-funcao ira retornar:
-1 - a quantidade de numeros cadastrados
-2 - o maior numero cadastrado
-3 - o menor numero cadastrado
-4 - a soma de todos os valores
-5 - a media dos valores
-*/
+const num = document.querySelector('input#value');
+const list = document.querySelector('select#list');
+const res = document.querySelector('div.answers');
+const numArr = [];
 
-// Adiciona ao Select os valores informados no Input
-function numbersToList(num) {
-  num = document.querySelector('input#value').value;
-  const list = document.querySelector('select#list');
-  const listedNumber = document.createElement('option');
-  listedNumber.value = `${num}`;
-  listedNumber.innerText += `Number ${num} was added.`;
-  list.appendChild(listedNumber);
+// Confere se o número informado no Input está entre os valores esperados
+function checkNumber(n) {
+  if (n >= 1 && n <= 100) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
+// Confere se o número informado no Input não está duplicado
+function checkList(n, l) {
+  if (l.indexOf(n) != -1) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+// Adiciona ao Select os valores informados no Input
+function addNumbers() {
+  if (checkNumber(num.value) && !checkList(num.value, numArr)) {
+    numArr.push(num.value);
+    const listedNumber = document.createElement('option');
+    listedNumber.value = `${num.value}`;
+    listedNumber.className = 'listed-number';
+    listedNumber.innerText += `Number ${num.value} was added.`;
+    list.appendChild(listedNumber);
+  } else {
+    alert('[Error] The number is invalid or it is already in the list.');
+  }
+  num.value = '';
+}
+
+/*
+Verifica todos os números informados no Input e retorna:
+1 - A quantidade de números;
+2 - O maior número;
+3 - O menor número;
+4 - A soma de todos os valores;
+5 - A média dos valores.
+*/
+function verify() {
+  const quantity = document.querySelector('p#quantity');
+  const highest = document.querySelector('p#highest');
+  const lowest = document.querySelector('p#lowest');
+  const sum = document.querySelector('p#sum');
+  const average = document.querySelector('p#average');
+  let x = 0;
+  numArr.sort();
+  for (let i = 0; i < numArr.length; i++) {
+    x += Number(numArr[i]);
+  }
+
+  quantity.innerText = `The quantity of entered numbers is ${numArr.length}.`;
+  highest.innerText = `The highest number is ${numArr.at(-1)}.`;
+  lowest.innerText = `The lowest number is ${numArr.at(0)}.`;
+  sum.innerText = `The sum of all numbers results in ${x}.`;
+  average.innerText = `The average value of all numbers is ${x / numArr.length}.`;
+}
